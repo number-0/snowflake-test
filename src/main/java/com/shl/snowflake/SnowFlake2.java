dea646c531944df580f8766eba3b6945952e9b81
songhengliang <songhengliang@51xf.cn>
2021-09-07 19:36:06 +0800

    snowflake

--- src/main/java/com/shl/snowflake/SnowFlake2.java
+++ src/main/java/com/shl/snowflake/SnowFlake2.java
@@ -2,11 +2,11 @@ package com.shl.snowflake;
 
 /**
  * twitter的snowflake算法 -- java实现
- * 
+ * 改动：5位机器标识和5位数据中心 合起来变成10位
  * @author beyond
  * @date 2016/11/26
  */
-public class SnowFlake {
+public class SnowFlake2 {
 
     /**
      * 起始的时间戳
@@ -17,36 +17,36 @@ public class SnowFlake {
      * 每一部分占用的位数
      */
     private final static long SEQUENCE_BIT = 12; //序列号占用的位数
-    private final static long MACHINE_BIT = 5;   //机器标识占用的位数
-    private final static long DATACENTER_BIT = 5;//数据中心占用的位数
+//    private final static long MACHINE_BIT = 5;   //机器标识占用的位数
+//    private final static long DATACENTER_BIT = 5;//数据中心占用的位数
+    private final static long MACHINE_IP_BIT = 10;   //机器ip标识占用的位数
 
     /**
      * 每一部分的最大值
      */
-    private final static long MAX_DATACENTER_NUM = -1L ^ (-1L << DATACENTER_BIT);
-    private final static long MAX_MACHINE_NUM = -1L ^ (-1L << MACHINE_BIT);
+//    private final static long MAX_DATACENTER_NUM = -1L ^ (-1L << DATACENTER_BIT);
+//    private final static long MAX_MACHINE_NUM = -1L ^ (-1L << MACHINE_BIT);
+    private final static long MAX_MACHINE_IP_NUM = -1L ^ (-1L << MACHINE_IP_BIT);
     private final static long MAX_SEQUENCE = -1L ^ (-1L << SEQUENCE_BIT);
+
 
     /**
      * 每一部分向左的位移
      */
-    private final static long MACHINE_LEFT = SEQUENCE_BIT;
-    private final static long DATACENTER_LEFT = SEQUENCE_BIT + MACHINE_BIT;
-    private final static long TIMESTMP_LEFT = DATACENTER_LEFT + DATACENTER_BIT;
+//    private final static long MACHINE_LEFT = SEQUENCE_BIT;
+//    private final static long DATACENTER_LEFT = SEQUENCE_BIT + MACHINE_BIT;
+    private final static long MACHINE_IP_LEFT = SEQUENCE_BIT;
+    private final static long TIMESTMP_LEFT = MACHINE_IP_LEFT + MACHINE_IP_BIT;
 
-    private long datacenterId;  //数据中心
+//    private long datacenterId;  //数据中心
     private long machineId;     //机器标识
     private long sequence = 0L; //序列号
     private long lastStmp = -1L;//上一次时间戳
 
-    public SnowFlake(long datacenterId, long machineId) {
-        if (datacenterId > MAX_DATACENTER_NUM || datacenterId < 0) {
-            throw new IllegalArgumentException("datacenterId can't be greater than " + MAX_DATACENTER_NUM + " or less than 0");
+    public SnowFlake2(long machineId) {
+        if (machineId > MAX_MACHINE_IP_NUM || machineId < 0) {
+            throw new IllegalArgumentException("machineId can't be greater than " + MAX_MACHINE_IP_NUM + " or less than 0");
         }
-        if (machineId > MAX_MACHINE_NUM || machineId < 0) {
-            throw new IllegalArgumentException("machineId can't be greater than " + MAX_MACHINE_NUM + " or less than 0");
-        }
-        this.datacenterId = datacenterId;
         this.machineId = machineId;
     }
 
@@ -79,8 +79,8 @@ public class SnowFlake {
         lastStmp = currStmp;
 
         return (currStmp - START_STMP) << TIMESTMP_LEFT //时间戳部分
-                | datacenterId << DATACENTER_LEFT       //数据中心部分
-                | machineId << MACHINE_LEFT             //机器标识部分
+//                | datacenterId << DATACENTER_LEFT       //数据中心部分
+                | machineId << MACHINE_IP_LEFT             //机器标识部分
                 | sequence;                             //序列号部分
     }
 
@@ -97,10 +97,11 @@ public class SnowFlake {
     }
 
     public static void main(String[] args) {
-        SnowFlake snowFlake = new SnowFlake(1, 1);
+        SnowFlake2 snowFlake = new SnowFlake2(1023);
 
         for (int i = 0; i < (1 << 12); i++) {
             System.out.println(snowFlake.nextId());
         }
+
     }
 }
